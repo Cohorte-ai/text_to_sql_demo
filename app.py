b7bf2b1cc58f905266e155865a7c5dc0ea02f6ae
Cmouzouni <charafeddine.mouzouni@gmail.com>
2024-04-15 14:39:00 +0100

    added some minor styling changes

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ from llama_index.core import SQLDatabase, ServiceContext
 
 from llama_index.core.query_engine import NLSQLTableQueryEngine
 
-#os.environ['OPENAI_API_KEY'] = st.secrets['OPENAI_API_KEY']
+os.environ['OPENAI_API_KEY'] = st.secrets['OPENAI_API_KEY']
 
 
 class StreamlitChatPack(BaseLlamaPack):
@@ -47,8 +47,12 @@ class StreamlitChatPack(BaseLlamaPack):
         **kwargs: Any,
     ) -> None:
         """Init params."""
-        
+
         self.page = page
+
+    def load_css(self, file_name):
+        with open(file_name) as css:
+            st.markdown(f'<style>{css.read()}</style>', unsafe_allow_html=True)
 
     def get_modules(self) -> Dict[str, Any]:
         """Get modules."""
@@ -65,14 +69,18 @@ class StreamlitChatPack(BaseLlamaPack):
             menu_items=None,
         )
 
+        # Load your CSS
+        self.load_css('style.css')
+
         # Load your image
         image = Image.open('logo.png')
         # Display the image in the sidebar at the top left
-        st.sidebar.image(image, width=70)
+        st.sidebar.image(image, width=40)
 
         if "messages" not in st.session_state:  # Initialize the chat messages history
             st.session_state["messages"] = [
-                {"role": "assistant", "content": f"Hello. Ask me anything related to the database."}
+                {"role": "assistant",
+                    "content": f"Hello. Ask me anything related to the database."}
             ]
 
         st.title(
@@ -83,9 +91,11 @@ class StreamlitChatPack(BaseLlamaPack):
             icon="ℹ️",
         )
         # Define the pills with emojis
-        query_options = ["None", "In a markdown table format show which users bought '4K LED Smart TV', their purchase date and their location", "In a markdown table show all the products bought under books category and their reviews", "Analyse all the reviews for Electronics category and list points of improvements in a table"]
+        query_options = ["None", "In a markdown table format show which users bought '4K LED Smart TV', their purchase date and their location",
+                         "In a markdown table show all the products bought under books category and their reviews", "Analyse all the reviews for Electronics category and list points of improvements in a table"]
         # emojis = ["👥", "📅", "🏷️"]
-        selected_query = pills("Select example queries or enter your own query in the chat input below", query_options, key="query_pills")
+        selected_query = pills(
+            "Select example queries or enter your own query in the chat input below", query_options, key="query_pills")
 
         def add_to_message_history(role, content):
             message = {"role": role, "content": str(content)}
@@ -98,24 +108,24 @@ class StreamlitChatPack(BaseLlamaPack):
             df = pd.read_sql_query(query, conn)
             return df
 
-        #@st.cache_resource
+        # @st.cache_resource
         def load_db_llm():
             # Load the SQLite database
-            #engine = create_engine("sqlite:///ecommerce_platform1.db")
-            engine = create_engine("sqlite:///ecommerce_platform1.db?mode=ro", connect_args={"uri": True})
+            # engine = create_engine("sqlite:///ecommerce_platform1.db")
+            engine = create_engine(
+                "sqlite:///ecommerce_platform1.db?mode=ro", connect_args={"uri": True})
 
-            sql_database = SQLDatabase(engine) #include all tables
+            sql_database = SQLDatabase(engine)  # include all tables
 
             # Initialize LLM
-            #llm2 = PaLM(api_key=os.environ["GOOGLE_API_KEY"])  # Replace with your API key
+            # llm2 = PaLM(api_key=os.environ["GOOGLE_API_KEY"])  # Replace with your API key
             Settings.llm = OpenAI(temperature=0.1, model="gpt-3.5-turbo-1106")
 
-            #Settings.embed_model = "local:BAAI/bge-base-en-v1.5"
-            
+            # Settings.embed_model = "local:BAAI/bge-base-en-v1.5"
+
             return sql_database, Settings, engine
 
         sql_database, service_context, engine = load_db_llm()
-
 
        # Sidebar for database schema viewer
         st.sidebar.markdown("## Database Schema Viewer")
@@ -131,19 +141,20 @@ class StreamlitChatPack(BaseLlamaPack):
 
         db_file = 'ecommerce_platform1.db'
         conn = sqlite3.connect(db_file)
-    
+
         # Display the selected table
         if selected_table:
             df = get_table_data(selected_table, conn)
             st.sidebar.text(f"Data for table '{selected_table}':")
             st.sidebar.dataframe(df)
-    
+
         # Close the connection
         conn.close()
-        
+
         st.sidebar.markdown('## Disclaimer')
-        st.sidebar.markdown("""This application is for demonstration purposes only and may not cover all aspects of real-world data complexities. Please use it as a guide and not as a definitive source for decision-making.""")
-    
+        st.sidebar.markdown(
+            """This application is for demonstration purposes only and may not cover all aspects of real-world data complexities. Please use it as a guide and not as a definitive source for decision-making.""")
+
         if "query_engine" not in st.session_state:  # Initialize the query engine
             st.session_state["query_engine"] = NLSQLTableQueryEngine(
                 sql_database=sql_database,
@@ -151,15 +162,17 @@ class StreamlitChatPack(BaseLlamaPack):
                 service_context=service_context
             )
 
-        for message in st.session_state["messages"]:  # Display the prior chat messages
+        # Display the prior chat messages
+        for message in st.session_state["messages"]:
             with st.chat_message(message["role"]):
                 st.write(message["content"])
 
         # Always show the chat input
-        prompt = st.chat_input("Enter your natural language query about the database")
+        prompt = st.chat_input(
+            "Enter your natural language query about the database")
 
         # If a pill is selected, override the chat input with the pill's value
-        if selected_query and selected_query!="None":
+        if selected_query and selected_query != "None":
             prompt = selected_query
 
         # Handle the prompt logic
@@ -172,12 +185,14 @@ class StreamlitChatPack(BaseLlamaPack):
         if st.session_state["messages"][-1]["role"] != "assistant":
             with st.spinner():
                 with st.chat_message("assistant"):
-                    response = st.session_state["query_engine"].query("User Question:"+prompt+". ")
+                    response = st.session_state["query_engine"].query(
+                        "User Question:"+prompt+". ")
                     sql_query = f"```sql\n{response.metadata['sql_query']}\n```\n**Response:**\n{response.response}\n"
                     response_container = st.empty()
                     response_container.write(sql_query)
                     # st.write(response.response)
                     add_to_message_history("assistant", sql_query)
+
 
 if __name__ == "__main__":
     StreamlitChatPack(run_from_main=True).run()
